04694672c7899dd664248b24b00e570c0ac9ee45
Antrall <vadik.kokhanenko@gmail.com>
2026-02-20 17:23:52 +0300

    add: register and login pages, main css and js files

---
+++ J3210/Коханенко Вадим/labs/lab1/js/main.js
@@ -0,0 +1,7 @@
+// Загрузка DOM и инициализация bootstrap
+document.addEventListener('DOMContentLoaded', function() {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
