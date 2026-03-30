4589762b7fe6a299a0a3027de545736ed7a9e4d9
Arsenii <arseniyjvagin@gmail.com>
2026-03-30 23:57:43 +0300

    feat: hw2, hw3, hw4 done: some changes to the footer and the site's appearance, removing unnecessary elements, increasing accessibility

--- J3212/Вагин Арсений/labs/lab2/js/main.js
+++ J3212/Вагин Арсений/labs/lab2/js/main.js
@@ -316,3 +316,21 @@ navLinks.forEach(link => {
         });
     }
 });
+
+// Смена темы
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggleBtn = document.getElementById('themeToggle');
+    const currentTheme = localStorage.getItem('theme') || 'light';
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (themeToggleBtn) {
+        themeToggleBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            let theme = document.documentElement.getAttribute('data-theme');
+            let newTheme = theme === 'dark' ? 'light' : 'dark';
+
+            document.documentElement.setAttribute('data-theme', newTheme);
+            localStorage.setItem('theme', newTheme);
+        });
+    }
+});
