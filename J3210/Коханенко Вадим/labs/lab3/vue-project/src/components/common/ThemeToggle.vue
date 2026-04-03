17ded4d90fdcfcecac611285e3b6d423a677bf21
Antrall <vadik.kokhanenko@gmail.com>
2026-04-03 12:51:53 +0300

    feat: main css styles and themes

---
+++ J3210/Коханенко Вадим/labs/lab3/vue-project/src/components/common/ThemeToggle.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="theme-toggle-btn" @click="toggleTheme">
+    <svg class="icon"><use xlink:href="/sprite.svg#icon-arrow-repeat"></use></svg>
+    {{ theme === 'light' ? 'Тёмная' : 'Светлая' }}
+  </button>
+</template>
+
+<script setup>
+import { useTheme } from '@/composables/useTheme'
+
+const { theme, toggleTheme } = useTheme()
+</script>
