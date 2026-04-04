8b71c4218eb9eb7a6e8c9be2ba31054f7d3da523
shlaapnik <artem.tema1000@gmail.com>
2026-04-04 15:22:34 +0300

    feat: init of lab2

---
+++ J3210/Марковский Артём/homeworks/hw234/project-management/js/init.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  void initializeApp();
+});
+
+async function initializeApp() {
+  syncProjects();
+
+  const pageName = getPageName();
+
+  if (!protectPages(pageName)) {
+    return;
+  }
+
+  setUserName();
+  markNavigation(pageName);
+  setupAuthForms();
+  setupLogout();
+
+  if (pageName === "dashboard" && typeof renderDashboard === "function") {
+    setupDashboardActions();
+    await renderDashboard();
+  }
+
+  if (pageName === "search" && typeof renderSearch === "function") {
+    await renderSearch();
+  }
+
+  if (pageName === "project" && typeof renderProject === "function") {
+    await renderProject();
+  }
+}
