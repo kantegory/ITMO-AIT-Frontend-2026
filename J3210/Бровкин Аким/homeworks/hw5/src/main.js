8b3b1f8a86c3e379e418b770b500b78a02df06e8
Akim Brovkin <102675010+seichikk@users.noreply.github.com>
2026-04-06 15:46:14 +0300

    feat: импортирован router

--- J3210/Бровкин Аким/homeworks/hw5/src/main.js
+++ J3210/Бровкин Аким/homeworks/hw5/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css";
+import router from './router'
 import "bootstrap";
-
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
