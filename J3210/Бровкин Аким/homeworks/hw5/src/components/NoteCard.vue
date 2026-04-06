b21c31f606ce9b3201ef3b82773406605d0aec59
seichikk <trchfk@gmail.com>
2026-04-06 15:16:36 +0300

    feat: создание дочернего компонента NoteCard

---
+++ J3210/Бровкин Аким/homeworks/hw5/src/components/NoteCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card mb-3 shadow-sm">
+    <div class="card-body">
+      <h5 class="card-title">{{ title }}</h5>
+      <p class="card-text">{{ content }}</p>
+      <button class="btn btn-outline-danger btn-sm" @click="$emit('delete-note')">
+        Удалить
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NoteCard',
+  props: {
+    title: String,
+    content: String
+  },
+  emits: ['delete-note']
+}
+</script>
