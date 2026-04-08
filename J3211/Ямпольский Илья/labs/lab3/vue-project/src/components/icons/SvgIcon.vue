3dc07c194d17cdaf0859512bca060e47673b2a45
Ilya Yampolskiy <hellboy@niuitmo.ru>
2026-04-09 00:30:58 +0300

    feat: иконки добавить

---
+++ J3211/Ямпольский Илья/labs/lab3/vue-project/src/components/icons/SvgIcon.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg :class="['icon', `icon--${size}`]" aria-hidden="true">
+    <use :href="`/svg/sprite.svg#${name}`" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'SvgIcon',
+  props: {
+    name: { type: String, required: true },
+    size: {
+      type: String,
+      default: 'md'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.icon {
+  width: 20px;
+  height: 20px;
+  fill: none;
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  vertical-align: middle;
+  display: inline-block;
+}
+</style>
