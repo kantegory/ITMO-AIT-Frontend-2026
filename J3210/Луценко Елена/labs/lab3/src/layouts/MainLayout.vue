9d7ccf01bd0ea0fabc7f962af16c6899e029a6c5
lenocheck <lenalutsenko2006@gmail.com>
2026-04-15 01:12:08 +0300

    feat: add layouts

---
+++ J3210/Луценко Елена/labs/lab3/src/layouts/MainLayout.vue
@@ -0,0 +1,10 @@
+<script setup>
+import AppNavbar from '@/components/layout/AppNavbar.vue'
+</script>
+
+<template>
+    <div class="main-layout">
+        <AppNavbar/>
+        <slot/>
+    </div>
+</template>
