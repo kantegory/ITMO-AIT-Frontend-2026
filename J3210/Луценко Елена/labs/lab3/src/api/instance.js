f4e58b03411d84e1f5d4ab304803289484b7cdf4
lenocheck <lenalutsenko2006@gmail.com>
2026-04-15 01:18:55 +0300

    feat: add database

---
+++ J3210/Луценко Елена/labs/lab3/src/api/instance.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const apiURL = 'http://localhost:3000'
+const instance = axios.create({
+    baseURL: apiURL,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+
+instance.interceptors.request.use(config => {
+    const token = localStorage.getItem('accessToken')
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+})
+export default instance
