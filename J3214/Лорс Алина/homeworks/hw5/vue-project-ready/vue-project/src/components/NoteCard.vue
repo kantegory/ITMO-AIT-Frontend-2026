5519be62316e38a15f829a1a33d8039a989e80a0
AlinaMors <91571078+AlinaMors@users.noreply.github.com>
2026-04-30 23:30:53 +0300

    Lors_j3214_hw5

---
+++ J3214/Лорс Алина/homeworks/hw5/vue-project-ready/vue-project/src/components/NoteCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <article class="card h-100">
+    <div class="card-body">
+      <h3 class="card-title h5">
+        {{ title }}
+      </h3>
+
+      <p class="card-text">
+        {{ text }}
+      </p>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'NoteCard',
+
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
