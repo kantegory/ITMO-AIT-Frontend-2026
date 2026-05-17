ee232f395c853aa9e4115e8c6f86cb44c6c70217
Mikhail Shperling <mikhailshperling@MacBook-Pro-Mikhail.local>
2026-05-17 23:07:42 +0300

    lab3 >  hw5

---
+++ J3210/Михаил Шперлинг/homeworks/hw5/ai-hub/src/layouts/BaseLayout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+</script>
+
+<template>
+  <div>
+    <nav-bar />
+    <div class="container mt-4 mb-5">
+      <slot />
+    </div>
+  </div>
+</template>
