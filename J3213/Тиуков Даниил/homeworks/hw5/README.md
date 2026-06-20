b07d9e36d186785905aadae41cb4272d25193529
Daniil Tiucov <splixio86@gmail.com>
2026-06-20 21:40:25 +0300

    ДЗ5: Vue-приложение

---
+++ J3213/Тиуков Даниил/homeworks/hw5/README.md
@@ -0,0 +1,19 @@
+# ДЗ: изучение NPM и запуск проекта на Vue
+
+## Стек
+- Vue 3
+- Vite
+
+## Запуск
+
+```sh
+npm install
+npm run dev
+```
+
+## Сборка
+
+```sh
+npm run build
+npm run preview
+```
---
+++ J3213/Тиуков Даниил/homeworks/hw5/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="container">
+    <h1>Hello, Vue 3!</h1>
+    <p>Проект создан и запущен через Vite.</p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
